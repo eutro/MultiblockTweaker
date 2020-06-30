7cdd380bbe2abff685a994fc26deac062270b1fc
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-06-30 06:09:58 +0100

    Improve JEI recipe height calculations.

--- src/main/java/eutros/multiblocktweaker/gregtech/recipes/RecipeMapMultiblock.java
+++ src/main/java/eutros/multiblocktweaker/gregtech/recipes/RecipeMapMultiblock.java
@@ -5,44 +5,71 @@ import gregtech.api.capability.impl.FluidTankList;
 import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
 import gregtech.api.gui.widgets.ProgressWidget;
+import gregtech.api.recipes.Recipe;
 import gregtech.api.recipes.RecipeMap;
 import net.minecraftforge.items.IItemHandlerModifiable;
-import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.Nonnull;
+import java.util.Set;
 import java.util.function.DoubleSupplier;
 
 public class RecipeMapMultiblock extends RecipeMap<CustomRecipeBuilder> {
 
-    public static final int DEFAULT_HEIGHT = 166;
-    public static final int BASE_UI_HEIGHT = 144;
     public static final int SLOT_SIZE = 18;
     public static final int GRID_WIDTH = 3;
-    public static final int BASE_SLOT_Y_BOTTOM = 134;
-    private final int UI_HEIGHT;
+    private final int slotsHeight;
 
     public RecipeMapMultiblock(String unlocalizedName, int minInputs, int maxInputs, int minOutputs, int maxOutputs, int minFluidInputs, int maxFluidInputs, int minFluidOutputs, int maxFluidOutputs, CustomRecipeBuilder defaultRecipe) {
         super(unlocalizedName, minInputs, maxInputs, minOutputs, maxOutputs, minFluidInputs, maxFluidInputs, minFluidOutputs, maxFluidOutputs, defaultRecipe);
-        UI_HEIGHT = MathHelper.max(
-                DEFAULT_HEIGHT,
-                BASE_UI_HEIGHT + (maxFluidInputs / GRID_WIDTH + maxOutputs / GRID_WIDTH) * SLOT_SIZE / 2,
-                BASE_UI_HEIGHT + (maxFluidInputs / GRID_WIDTH + maxFluidOutputs / GRID_WIDTH) * SLOT_SIZE / 2
+
+        slotsHeight = MathHelper.max(
+                64,
+                getSlotsHeight(maxInputs, maxFluidInputs),
+                getSlotsHeight(maxOutputs, maxFluidOutputs)
         );
     }
 
-    @Override
-    public ModularUI.@NotNull Builder createUITemplate(DoubleSupplier progressSupplier, IItemHandlerModifiable importItems, IItemHandlerModifiable exportItems, FluidTankList importFluids, FluidTankList exportFluids) {
-        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BACKGROUND, 176, UI_HEIGHT);
-        builder.widget(new ProgressWidget(progressSupplier, 77, UI_HEIGHT - BASE_UI_HEIGHT, 20, 20, this.progressBarTexture, this.moveType));
-        this.addInventorySlotGroup(builder, importItems, importFluids, false);
-        this.addInventorySlotGroup(builder, exportItems, exportFluids, true);
+    private static int getSlotsHeight(int items, int fluids) {
+        int[] grid = determineSlotsGrid(items);
+        return SLOT_SIZE * (
+                grid[1] + (
+                        fluids > 0 ? (
+                                grid[1] >= fluids && grid[0] < 3 ?
+                                fluids - 1 :
+                                (int) Math.ceil(fluids / (double) GRID_WIDTH)
+                        ) :
+                        0
+                )
+        ) + 10;
+    }
+
+    @Nonnull
+    public ModularUI.Builder createUITemplate(DoubleSupplier progressSupplier, IItemHandlerModifiable importItems, IItemHandlerModifiable exportItems, FluidTankList importFluids, FluidTankList exportFluids) {
+        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BACKGROUND,
+                176,
+                (
+                        slotsHeight +
+                                (
+                                        getRecipeList().parallelStream()
+                                                .map(Recipe::getPropertyKeys)
+                                                .mapToInt(Set::size)
+                                                .max()
+                                                .orElse(0) + 4
+                                ) * 10
+                ) * 3 / 2); // For some godforsaken reason the JEI category multiplies by 2/3
+        builder.widget(new ProgressWidget(progressSupplier, 77, slotsHeight / 2 - 10, 20, 20, progressBarTexture, moveType));
+        addInventorySlotGroup(builder, importItems, importFluids, false);
+        addInventorySlotGroup(builder, exportItems, exportFluids, true);
         return builder;
     }
 
+    // what is even going on here
+    @SuppressWarnings("ConstantConditions")
     protected void addInventorySlotGroup(ModularUI.Builder builder, IItemHandlerModifiable itemHandler, FluidTankList fluidHandler, boolean isOutputs) {
         int itemInputsCount = itemHandler.getSlots();
         int fluidInputsCount = fluidHandler.getTanks();
         boolean invertFluids = false;
-        if (itemInputsCount == 0) {
+        if(itemInputsCount == 0) {
             int tmp = itemInputsCount;
             itemInputsCount = fluidInputsCount;
             fluidInputsCount = tmp;
@@ -51,29 +78,32 @@ public class RecipeMapMultiblock extends RecipeMap<CustomRecipeBuilder> {
         int[] inputSlotGrid = determineSlotsGrid(itemInputsCount);
         int itemSlotsToLeft = inputSlotGrid[0];
         int itemSlotsToDown = inputSlotGrid[1];
-        int startInputsX = isOutputs ? 106 : 69 - itemSlotsToLeft * SLOT_SIZE;
-        int startInputsY = UI_HEIGHT - BASE_SLOT_Y_BOTTOM - (int) (itemSlotsToDown / 2.0 * SLOT_SIZE);
-        for (int i = 0; i < itemSlotsToDown; i++) {
-            for (int j = 0; j < itemSlotsToLeft; j++) {
+        int startInputsX = isOutputs ? 106 : 69 - itemSlotsToLeft * 18;
+        //             replace hard-coded 32...
+        int startInputsY = slotsHeight / 2 - (int) (itemSlotsToDown / 2.0 * SLOT_SIZE);
+        for(int i = 0; i < itemSlotsToDown; i++) {
+            for(int j = 0; j < itemSlotsToLeft; j++) {
                 int slotIndex = i * itemSlotsToLeft + j;
-                int x = startInputsX + SLOT_SIZE * j;
-                int y = startInputsY + SLOT_SIZE * i;
+                int x = startInputsX + 18 * j;
+                int y = startInputsY + 18 * i;
                 addSlot(builder, x, y, slotIndex, itemHandler, fluidHandler, invertFluids, isOutputs);
             }
         }
-        if (fluidInputsCount > 0 || invertFluids) {
-            if (itemSlotsToDown >= fluidInputsCount && itemSlotsToLeft < GRID_WIDTH) {
-                int startSpecX = isOutputs ? startInputsX + itemSlotsToLeft * SLOT_SIZE : startInputsX - SLOT_SIZE;
-                for (int i = 0; i < fluidInputsCount; i++) {
-                    int y = startInputsY + SLOT_SIZE * i;
-                    addSlot(builder, startSpecX, y, i, itemHandler, fluidHandler, true, isOutputs);
+        if(fluidInputsCount > 0 || invertFluids) {
+            if(itemSlotsToDown >= fluidInputsCount && itemSlotsToLeft < 3) {
+                int startSpecX = isOutputs ? startInputsX + itemSlotsToLeft * 18 : startInputsX - 18;
+                for(int i = 0; i < fluidInputsCount; i++) {
+                    int y = startInputsY + 18 * i;
+                    addSlot(builder, startSpecX, y, i, itemHandler, fluidHandler, !invertFluids, isOutputs);
                 }
             } else {
-                int startSpecY = startInputsY + itemSlotsToDown * SLOT_SIZE;
-                for (int i = 0; i < fluidInputsCount; i++) {
-                    int x = isOutputs ? startInputsX + SLOT_SIZE * (i % GRID_WIDTH) : startInputsX + (itemSlotsToLeft - (i % GRID_WIDTH) - 1) * SLOT_SIZE;
-                    int y = startSpecY + (i / GRID_WIDTH) * SLOT_SIZE;
-                    addSlot(builder, x, y, i, itemHandler, fluidHandler, true, isOutputs);
+                int startSpecY = startInputsY + itemSlotsToDown * 18;
+                for(int i = 0; i < fluidInputsCount; i++) {
+                    int x = isOutputs ?
+                            startInputsX + 18 * (i % 3) :
+                            startInputsX + itemSlotsToLeft * 18 - 18 - 18 * (i % 3);
+                    int y = startSpecY + (i / 3) * 18;
+                    addSlot(builder, x, y, i, itemHandler, fluidHandler, !invertFluids, isOutputs);
                 }
             }
         }
