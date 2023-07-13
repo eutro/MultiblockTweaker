78bb3d03d9d83e749af492a1fdf66ec19868ddcc
alongstringofnumbers <sjensen793@gmail.com>
2023-07-12 18:04:30 -0700

    Update to use CEu buildscript

--- src/main/java/eutros/multiblocktweaker/MultiblockTweaker.java
+++ src/main/java/eutros/multiblocktweaker/MultiblockTweaker.java
@@ -1,13 +1,13 @@
 package eutros.multiblocktweaker;
 
-import eutros.MBTInternalTags;
+import com.myname.mymodid.MBTTags;
 import gregtech.GTInternalTags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 @Mod(modid = MultiblockTweaker.MOD_ID,
         name = "Multiblock Tweaker",
-        version = MBTInternalTags.VERSION,
+        version = MBTTags.VERSION,
         dependencies = GTInternalTags.DEP_VERSION_STRING)
 public class MultiblockTweaker {
 
